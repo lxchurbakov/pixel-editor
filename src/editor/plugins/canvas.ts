fc4a894bf2334a79a69b8520d3096a3a847acd32
lxchurbakov <lxchurbakov@gmail.com>
2023-12-30 18:51:56 +0300

    Add canvas plugin

---
+++ src/editor/plugins/canvas.ts
@@ -0,0 +1,39 @@
+import { EventEmitter } from '/src/libs/events';
+
+export type Point = { x: number, y: number };
+export type Rect = { width: number, height: number };
+
+export default class Canvas {
+    public rect: Rect;
+	public canvas: HTMLCanvasElement;
+	public context: CanvasRenderingContext2D;
+
+    public onRender = new EventEmitter<CanvasRenderingContext2D>();
+
+    constructor (public root: HTMLElement) {
+		const pixelRatio = window.devicePixelRatio || 1;
+
+        this.canvas = document.createElement('canvas');
+        this.rect = this.root.getBoundingClientRect();
+        
+		this.canvas.style.width = this.rect.width + 'px';
+		this.canvas.style.height = this.rect.height + 'px';
+		this.canvas.width = this.rect.width * pixelRatio;
+		this.canvas.height = this.rect.height * pixelRatio;
+
+        this.root.appendChild(this.canvas);
+
+        this.context = this.canvas.getContext('2d');
+		this.context.scale(pixelRatio, pixelRatio);
+
+        this.context.imageSmoothingEnabled = false;
+
+        requestAnimationFrame(this.render);
+    }
+
+	public render = () => {
+		this.context.clearRect(0, 0, this.rect.width, this.rect.height);
+		this.onRender.emitSync(this.context);
+		requestAnimationFrame(this.render);
+	};
+};
